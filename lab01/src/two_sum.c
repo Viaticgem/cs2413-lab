72055579afe610bf6de1eb4ef0312ff34ee28b5e
Hudson Craven <hudson.craven29@gmail.com>
2026-02-08 22:25:04 -0600

    Update two_sum.c

--- lab01/src/two_sum.c
+++ lab01/src/two_sum.c
@@ -1,14 +1,21 @@
-#include "two_sum.h"
+#include "two_sum_sorted.h"
 
-int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
+int two_sum_sorted(const int* nums, int n, int target, int* out_i, int* out_j) {
+    if (!nums || !out_i || !out_j || n < 2) return 0;
 
-    // TODO: implement Two Sum.
-    // Requirements:
-    //  - find i < j such that nums[i] + nums[j] == target
-    //  - store indices in *out_i and *out_j
-    //  - return 1 if found; otherwise return 0
+    // TODO: implement rwo sum solution for sorted arrays.
+    for (int i = 0; i < n-1; i++){
+        for (int j = i+1; j< n-1; j++) {
+            if(nums[i] + nums[j] == target){
+                out_i = nums[i];
+                out_j = nums[j];
+                return 1;
+            }
+
+        }
+    }
 
     return 0;
 }
+// think about the time complexity and space complexity of your solution
 
-//think about the time complexity and space complexity of your solution
