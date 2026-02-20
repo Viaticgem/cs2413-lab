ba82d2b7b2ad9055846101da774fe1add8f82533
Hudson Craven <hudson.craven29@gmail.com>
2026-02-19 20:30:22 -0600

    Update student.c

--- Assignment1/Question1/student.c
+++ Assignment1/Question1/student.c
@@ -15,8 +15,29 @@
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 
- // TODO: implement
+    int i = m - 1;       // last real element in nums1
+    int j = nums2Size - 1;   // last element in nums2
+    int k = nums1Size - 1;   // last position in nums1
+    int place = n + m - 1;   // last position in nums1
 
+    // Merge from the back
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+        place--;
+    }
 
-
+    // If nums2 still has elements left
+    while (j >= 0) {
+        nums1[place] = nums2[j];
+        j--;
+        k--;
+        place--;
+    }
 }
