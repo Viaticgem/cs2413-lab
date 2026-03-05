69edc30e45a278f45fbcea47730acb9934aacf81
Hudson Craven <hudson.craven29@gmail.com>
2026-03-04 23:10:50 -0600

    Update student.c

--- lab03/Question2/student.c
+++ lab03/Question2/student.c
@@ -14,4 +14,33 @@
 
 struct ListNode* swapPairs(struct ListNode* head) {
       // TODO: implement
+      // list not big enough to swap.
+    if (head == NULL || head->next == NULL)
+        return head;
+
+    //intializing pointers 
+    struct ListNode* new = head->next; 
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+
+    //loops till there are no pairs left.
+    while (curr != NULL && curr->next != NULL) {
+       //next pair pointer and second value to swap
+        struct ListNode* second = curr->next;
+        struct ListNode* next = second->next;
+
+        second->next = curr;
+        curr->next = next;
+
+        //for initial value
+        if (prev != NULL)
+            prev->next = second;
+
+
+      //moves on to next pair
+        prev = curr;
+        curr = next;
+    }
+
+    return new; // returns switched pairs
 }
