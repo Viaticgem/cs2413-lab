3d885d5827373992051839538acb116038ac3463
Hudson Craven <hudson.craven29@gmail.com>
2026-03-04 23:11:53 -0600

    Update student.c

--- lab03/Question3/student.c
+++ lab03/Question3/student.c
@@ -14,5 +14,19 @@
 
 bool hasCycle(struct ListNode *head) {
     // TODO: implement
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+
+        slow = slow->next;          // move 1 step
+        fast = fast->next->next;    // move 2 steps
+
+        if (slow == fast) {
+            return true;            // cycle detected
+        }
+    }
+
+    return false;                   // reached end did not lap = no cycle
 
 }
