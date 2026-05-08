b5eb986c64761b6c10726a15d86341f87ea1c011
Hudson Craven <hudson.craven29@gmail.com>
2026-05-07 20:05:13 -0500

    Update student.c

--- lab07/Question1/student.c
+++ lab07/Question1/student.c
@@ -28,15 +28,45 @@ Notes:
 - You may write a helper function such as swap(...) if you want.
 */
 
+#include <stdbool.h>
+#include <stddef.h>
+
 void bubbleSort(int arr[], int size) {
-    // TODO: implement basic bubble sort
-    (void)arr;
-    (void)size;
+    if (arr == NULL || size <= 1) {
+        return;
+    }
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 void bubbleSortOptimized(int arr[], int size) {
-    // TODO: implement optimized bubble sort with early stopping
-    (void)arr;
-    (void)size;
+    if (arr == NULL || size <= 1) {
+        return;
+    }
+
+    for (int i = 0; i < size - 1; i++) {
+        bool swapped = false;
+
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
 }
 
