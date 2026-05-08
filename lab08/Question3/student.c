8ce8fe7ebc231613d3f79da1824cb22b63cc0d4e
Hudson Craven <hudson.craven29@gmail.com>
2026-05-07 20:06:27 -0500

    Update student.c

--- lab08/Question3/student.c
+++ lab08/Question3/student.c
@@ -23,7 +23,18 @@ Hint:
 - This can be solved using two pointers.
 */
 
-bool isSubsequence(char* s, char* t) {
-    // TODO: implement
 
+
+bool isSubsequence(char* s, char* t) {
+    int i = 0;
+    int j = 0;
+
+    while (s[i] != '\0' && t[j] != '\0') {
+        if (s[i] == t[j]) {
+            i++;
+        }
+        j++;
+    }
+
+    return s[i] == '\0';
 }
