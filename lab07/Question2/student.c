42d56608b91e7908246a2c580621d7592725dd8d
Hudson Craven <hudson.craven29@gmail.com>
2026-05-07 20:05:31 -0500

    Update student.c

--- lab07/Question2/student.c
+++ lab07/Question2/student.c
@@ -28,9 +28,67 @@ Notes:
 - You may use temporary arrays inside your merge function.
 */
 
+void merge(int arr[], int left, int mid, int right) {
+    int leftSize = mid - left + 1;
+    int rightSize = right - mid;
+
+    int leftArr[leftSize];
+    int rightArr[rightSize];
+
+    for (int i = 0; i < leftSize; i++) {
+        leftArr[i] = arr[left + i];
+    }
+
+    for (int i = 0; i < rightSize; i++) {
+        rightArr[i] = arr[mid + 1 + i];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < leftSize && j < rightSize) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < leftSize) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < rightSize) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSortHelper(int arr[], int left, int right) {
+    if (left >= right) {
+        return;
+    }
+
+    int mid = left + (right - left) / 2;
+
+    mergeSortHelper(arr, left, mid);
+    mergeSortHelper(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+}
+
 void mergeSort(int arr[], int size) {
-    // TODO: implement merge sort
-    (void)arr;
-    (void)size;
+    if (size <= 1) {
+        return;
+    }
+
+    mergeSortHelper(arr, 0, size - 1);
 }
 
