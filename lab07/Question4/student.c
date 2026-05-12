4c1fd4798b84d9d63ed8c4f44a8ebd073efa4027
Hudson Craven <hudson.craven29@gmail.com>
2026-05-11 22:34:13 -0500

    Update student.c

--- lab07/Question4/student.c
+++ lab07/Question4/student.c
@@ -20,7 +20,17 @@ Notes:
 */
 
 void insertionSort(int arr[], int size) {
-    // TODO: implement insertion sort
-    (void)arr;
-    (void)size;
+    int i, j, key;
+
+    for (i = 1; i < size; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
 }
